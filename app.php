013b8c1fe9d04f063f509c396471827c4eba4fdd
VictorShark <47236884+VictorShark@users.noreply.github.com>
2026-01-12 22:56:03 -0300

    Update server details in app.php response

--- app.php
+++ app.php
@@ -1,23 +1,15 @@
 <?php
-// NÃO use header()
-// NÃO use echo com texto extra
-// NÃO use json_encode com flags estranhas
-
-// ignora tudo que o app manda
-// o app NÃO valida o request
-
 $response =
 '{"servers":[
   {
-    "name":"Servidor Oficial",
+    "name":"GTA SGNTRJ - GMNOVA",
     "ip":"181.214.48.174",
     "port":7777,
     "online":true,
-    "players":123,
+    "players":100,
     "maxplayers":500
   }
 ]}';
 
-// resposta FINAL precisa ser Base64 puro
 echo base64_encode($response);
 exit;

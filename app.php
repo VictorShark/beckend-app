d3c32bb343411786d966d17809063b20f01db476
VictorShark <47236884+VictorShark@users.noreply.github.com>
2026-01-12 23:35:48 -0300

    Refactor server response handling in app.php

--- app.php
+++ app.php
@@ -1,15 +1,29 @@
 <?php
-$response =
-'{"servers":[
-  {
-    "name":"GTA SGNTRJ - GMNOVA",
-    "ip":"181.214.48.174",
-    "port":7777,
-    "online":true,
-    "players":100,
-    "maxplayers":500
-  }
-]}';
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(404);
+    exit;
+}
 
+if (!isset($_POST['token'])) {
+    http_response_code(404);
+    exit;
+}
+
+// o app NÃO valida o conteúdo do token
+// ele só exige que exista
+$servers = [
+    "servers" => [
+        [
+            "name" => "GTA SGNTRJ - GMNOVA",
+            "ip" => "181.214.48.174",
+            "port" => 7777,
+            "online" => true,
+            "players" => 100,
+            "maxplayers" => 500
+        ]
+    ]
+];
+
+// resposta PRECISA ser Base64
+$response = json_encode($servers);
 echo base64_encode($response);
-exit;
